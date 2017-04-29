afb6807eb13eb16555d0e1acb94c559df8bf2025
Christian Galea <galeaettu@gmail.com>
2017-04-30 01:25:43 +0200

    First Commit, start of Fisher-Yates shuffle

---
+++ Assignment 1/IRandomProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    /// <summary>
+    /// An interface representing a Random Number Generator
+    /// </summary>
+    public interface IRandomProvider
+    {
+        int Next(int maxValue);
+    }
+}
