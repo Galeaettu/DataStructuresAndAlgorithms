98cf9783292baf23cd1d54096ef2780901565297
Christian Galea <galeaettu@gmail.com>
2017-05-06 17:16:33 +0200

    Bucket Sort

---
+++ Assignment 1/BucketSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class BucketSort
+    {
+        /// <summary>
+        /// Adapted from : https://www.programmingalgorithms.com/algorithm/bucket-sort
+        /// </summary>
+        /// <param name="data">An array of numbers to be sorted</param>
+        public static void BucketSort(ref long[] data)
+        {
+            long minValue = data[0];
+            long maxValue = data[0];
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (data[i] > maxValue)
+                    maxValue = data[i];
+                if (data[i] < minValue)
+                    minValue = data[i];
+            }
+
+            List<long>[] bucket = new List<long>[maxValue - minValue + 1];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<long>();
+            }
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                bucket[data[i] - minValue].Add(data[i]);
+            }
+
+            int k = 0;
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                if (bucket[i].Count > 0)
+                {
+                    for (int j = 0; j < bucket[i].Count; j++)
+                    {
+                        data[k] = bucket[i][j];
+                        k++;
+                    }
+                }
+            }
+        }
+    }
+}
